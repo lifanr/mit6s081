fe700d5bee8154ffc8ce53e568295698dcf7fb58
rikka <13253031112@163.com>
2022-02-23 19:37:53 +0800

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,37 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    int parent_fd[2];
+    int child_fd[2];
+    parent_fd= pipe(parent_fd[2]);
+    child_fd= pipe(child_fd[2]);
+    if(parent_fd== -1){
+        printf("pipe1 error");
+    }
+    if(child_fd== -1){
+        printf("pipe2 error");
+    }
+    int pid = fork();
+    char buf[64];
+    if(pid == 0){ //child
+        read(parent_fd[0], buf, 4);
+        printf("%d: received %s\n", getpid(), buf);
+        write(child_fd[1], "ping", strlen('ping'));
+    }else{
+        // parent
+        write(parent_fd[1], "pang", strlen("pang"));
+        read(child_fd[0], buf, 4);
+        printf("%d: received %s\n", getpid(), buf);
+    }
+    close(parent_fd[0]);
+    close(parent_fd[1]);
+    close(child_fd[0]);
+    close(child_fd[1]);
+    exit(0);
+
+//
+// Created by rikka on 2022/2/23.
+//
+
