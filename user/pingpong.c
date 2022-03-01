23d2ff803fd1f11c8be3a6809d5a6b339234e340
rikka <13253031112@163.com>
2022-03-01 15:23:54 +0800

    ..

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,8 +1,3 @@
-作者：lemonman
-        链接：https://zhuanlan.zhihu.com/p/272199762
-来源：知乎
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
